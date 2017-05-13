ab15e08d8995a1526721ca38fc5515844769b91c
firmiana <firmiana-caesar@outlook.com>
2017-05-13 22:48:04 +0800

    a templet for solving algorithm problem

---
+++ 算法题模板.cpp
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////////////
+//description：				      
+//input：		       
+//output:        
+//sample input：					     	
+//sample output：
+/////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namesapce std;
+
+int main()
+{	
+	//input your code from here
+	return 0;
+}
